dfe633abe7a13fc7d11c04d4b6f6d67e48df565d
FellipeCavalcante <azevedoaranha1@gmail.com>
2023-08-03 16:07:50 -0300

    troquei a mensagem

--- Comanf.java
+++ Comanf.java
@@ -1,5 +1,5 @@
     public class Comanf {
         public static void main(String [] args) {
-            System.out.println("Ola mundo!");
+            System.out.println("Fellipin!");
         }    
     }
